a492a7c8691e5ba3d94b07e793c4252e13529028
alvmiller <134068320+alvmiller@users.noreply.github.com>
2023-11-07 14:13:56 +0200

    Update factorials.cpp

--- factorials.cpp
+++ factorials.cpp
@@ -4,49 +4,60 @@
 //------------------------------------------------------------------------------
 inline unsigned int factorial00(unsigned int n)
 {
-    return (n == 0) ? 1 : n * factorial00(n - 1);
+    unsigned int result = 1;
+    for (unsigned int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-unsigned int tail_recursive_factorial01_core(unsigned int n, unsigned int a)
+inline unsigned int factorial01(unsigned int n)
+{
+    return (n == 0) ? 1 : n * factorial01(n - 1);
+}
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+unsigned int tail_recursion_factorial02_core(unsigned int n, unsigned int a)
 {
     if (n <= 1) {
         return a;
     }
 
-    return tail_recursive_factorial01_core(n - 1, n * a);
+    return tail_recursion_factorial02_core(n - 1, n * a);
 }
 
-unsigned int factorial01(unsigned int n)
+unsigned int factorial02(unsigned int n)
 {
-    return tail_recursive_factorial01_core(n, 1);
+    return tail_recursion_factorial02_core(n, 1);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 template <unsigned int n>
-struct factorial02
+struct factorial03
 {
-    enum { value = n * factorial02<n - 1>::value };
+    enum { value = n * factorial03<n - 1>::value };
 };
 
 template <>
-struct factorial02<1>
+struct factorial03<1>
 {
     enum { value = 1 };
 };
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-constexpr unsigned int factorial03(unsigned int n)
+constexpr unsigned int factorial04(unsigned int n)
 {
-    return (n == 0) ? 1 : n * factorial03(n - 1);
+    return (n == 0) ? 1 : n * factorial04(n - 1);
 }
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-constexpr unsigned int factorial04(unsigned int n)
+constexpr unsigned int factorial05(unsigned int n)
 {
     unsigned int result = 1;
     for (unsigned int i = 1; i <= n; ++i) {
@@ -57,9 +68,9 @@ constexpr unsigned int factorial04(unsigned int n)
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-consteval unsigned int factorial05(unsigned int n)
+consteval unsigned int factorial06(unsigned int n)
 {
-    return (n == 0) ? 1 : n * factorial03(n - 1);
+    return (n == 0) ? 1 : n * factorial06(n - 1);
 }
 //------------------------------------------------------------------------------
 
@@ -67,15 +78,16 @@ int main()
 {
     assert(factorial00(5) == 120);
     assert(factorial01(5) == 120);
-    assert(factorial02<5>::value == 120);
-    assert(factorial03(5) == 120);
+    assert(factorial02(5) == 120);
+    assert(factorial03<5>::value == 120);
     assert(factorial04(5) == 120);
     assert(factorial05(5) == 120);
+    assert(factorial06(5) == 120);
 
-    static_assert(factorial02<5>::value == 120, "factorial02 failed value");
-    static_assert(factorial03(5) == 120, "factorial03 failed value");
-    static_assert(factorial04(5) == 120, "factorial04 failed value");\
-    static_assert(factorial05(5) == 120, "factorial04 failed value");
+    static_assert(factorial03<5>::value == 120, "factorial03 failed value");
+    static_assert(factorial04(5) == 120, "factorial04 failed value");
+    static_assert(factorial05(5) == 120, "factorial05 failed value");
+    static_assert(factorial06(5) == 120, "factorial06 failed value");
 
     return 0;
 }
